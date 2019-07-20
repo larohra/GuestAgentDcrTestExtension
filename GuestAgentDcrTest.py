1ff5849f320772ced5b34915b36733a3a0a57a57
larohra <larohra@microsoft.com>
2019-07-19 17:57:02 -0700

    Added an operations.log for more operations logging

--- GuestAgentDcrTest.py
+++ GuestAgentDcrTest.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 from Utils.WAAgentUtil import waagent
 import Utils.HandlerUtil as Util
 import sys
 import re
 import traceback
+import os
 
-ExtensionShortName = "SampleExtension"
+ExtensionShortName = "GADcrTestExt"
+OperationFileName = "operations.log"
 
 
 def install():
     operation = "install"
     status = "success"
-    msg = "Installed successfully."
+    msg = "Installed successfully"
 
     hutil = parse_context(operation)
     hutil.log("Start to install.")
@@ -26,14 +29,18 @@ def enable():
     status = "success"
     msg = "Enabled successfully."
 
+    # Operations.append(operation)
     hutil = parse_context(operation)
     hutil.log("Start to enable.")
     public_settings = hutil.get_public_settings()
     name = public_settings.get("name")
     if name:
-        hutil.log("Hello {0}".format(name))
+        name = "Name: Hello {0}".format(name)
+        hutil.log(name)
+        print(name)
     else:
         hutil.error("The name in public settings is not provided.")
+    # msg = msg % ','.join(Operations)
     hutil.log(msg)
     hutil.do_exit(0, operation, status, '0', msg)
 
@@ -43,8 +50,10 @@ def disable():
     status = "success"
     msg = "Disabled successfully."
 
+    # Operations.append(operation)
     hutil = parse_context(operation)
     hutil.log("Start to disable.")
+    # msg % ','.join(Operations)
     hutil.log(msg)
     hutil.do_exit(0, operation, status, '0', msg)
 
@@ -54,8 +63,10 @@ def uninstall():
     status = "success"
     msg = "Uninstalled successfully."
 
+    # Operations.append(operation)
     hutil = parse_context(operation)
     hutil.log("Start to uninstall.")
+    # msg % ','.join(Operations)
     hutil.log(msg)
     hutil.do_exit(0, operation, status, '0', msg)
 
@@ -65,8 +76,10 @@ def update():
     status = "success"
     msg = "Updated successfully."
 
+    # Operations.append(operation)
     hutil = parse_context(operation)
     hutil.log("Start to update.")
+    # msg % ','.join(Operations)
     hutil.log(msg)
     hutil.do_exit(0, operation, status, '0', msg)
 
@@ -74,6 +87,9 @@ def update():
 def parse_context(operation):
     hutil = Util.HandlerUtility(waagent.Log, waagent.Error)
     hutil.do_parse_context(operation)
+    op_log = os.path.join(hutil.get_log_dir(), OperationFileName)
+    with open(op_log, 'a+') as oplog_handler:
+        oplog_handler.write("Operation: {0}; Seq No: {1}\n".format(operation, hutil.get_seq_no()))
     return hutil
 
 
